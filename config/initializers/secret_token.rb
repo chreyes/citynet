56ba9dd25385d56639d56ace8dc100f7d2cac48f
chreyes <chreyes@gmail.com>
2013-08-26 16:11:38 -0400

    funcionando login y mant usuarios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevisePruebas3::Application.config.secret_key_base = '8fece8a24e15225a3be38bf596418e79a0cbc1c7b10d6b87d71739d2d6ae3c6979468edb8592a2cb874d09eb9aa6a26e235e489ffd4dadda6dcb1d23bf8636ff'
